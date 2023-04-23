d62ddb54cdc478e68e582d7da9d477de693951fe
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-23 16:50:38 +0200

    adjust server config

--- config/index.js
+++ config/index.js
@@ -1,33 +1,35 @@
 // We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+const express = require('express');
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+const logger = require('morgan');
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
-const cors = require("cors"); // <== IMPORT
+const cors = require('cors'); // <== IMPORT
+
+const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:5173';
 
 // Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like heroku use something called a proxy and you need to add this to your server
-  app.set("trust proxy", 1);
+        // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+        // Services like heroku use something called a proxy and you need to add this to your server
+        app.set('trust proxy', 1);
 
-  app.use(
-    cors({
-      origin: ["http://localhost:3000"],
-    })
-  );
+        app.use(
+                cors({
+                        origin: [FRONTEND_URL],
+                })
+        );
 
-  // In development environment the app logs
-  app.use(logger("dev"));
+        // In development environment the app logs
+        app.use(logger('dev'));
 
-  // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+        // To have access to `body` property in the request
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
 };
